4ea9204c96b00024b852c3fa81b4a1566fa8750b
Jacob K <jacobknighj@gmail.com>
2021-11-04 16:34:34 +1100

    init

---
+++ app/controllers/ferrets_controller.rb
@@ -0,0 +1,10 @@
+class FerretsController < ApplicationController
+  def index
+  end
+  def show
+  end
+  def new
+  end
+  def edit
+  end
+end
---
+++ test/controllers/ferrets_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class FerretsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
